ccafdaadc47be0c701e3439e6dfd24b913df61c8
Amira <amira.mohamd791@gmail.com>
2024-05-06 16:43:09 +0300

    Amira part

---
+++ src/app/Account/logout/logout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { AccountService } from '../../Services/account.service';
+
+@Component({
+  selector: 'app-logout',
+  standalone: true,
+  imports: [],
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.css'
+})
+export class LogoutComponent {
+  constructor(public accountservice:AccountService){}
+  logout(){
+    this.accountservice.logout();
+  }
+}
